3d927da42d77a280073ec345296209726c79ec0d
MSI-SOBREMESA <git@bitbucket.org:plaroser/pruebaandroid.git>
2018-02-03 09:51:46 +0100

    Mono hecho

--- src/Models/Mono.java
+++ src/Models/Mono.java
@@ -1,5 +1,15 @@
 package Models;
 
 public class Mono {
+	private ListaLetra listaLetra;
 
+	public Mono() {
+		super();
+		this.listaLetra = new ListaLetra();
+	}
+	
+	public Character pulsarTecla() {
+		return listaLetra.getRandom();
+	}
+	
 }
